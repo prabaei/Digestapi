29db59c01f5f10756770458d2a8f0eb37a50213a
prabaei <prabaei@gmail.com>
2017-06-07 16:08:19 +0530

    attritest project created

---
+++ DigestApi/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace DigestApi.Controllers
+{
+    public class TestController : ApiController
+    {
+        [Authorize]
+        public JToken get()
+        {
+            return JObject.Parse("{Name:\"prabaharan\",age:3}");
+        }
+    }
+}
