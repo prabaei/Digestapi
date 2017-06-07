073d4bbb716a3f82ce9bd1bcbdd2b0b4bebee807
prabaei <prabaei@gmail.com>
2017-06-07 17:34:36 +0530

    testAttrib console added

---
+++ testAttrib/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testAttrib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("test attrib");
+            rojer rojerj = new rojer();
+        }
+    }
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TestAttribi : Attribute
+    {
+        public string message { get; set; }
+        public TestAttribi()
+        {
+            Console.Write("don't worry");
+        }
+        
+    }
+
+    [TestAttribi(message ="test message")]
+    public class rojer
+    {
+
+    }
+}
